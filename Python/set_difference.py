35643c2fac85896b222a710f124af17aecad6153
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2023-02-27 20:38:37 +0100

    Added set_intersection and set_difference

---
+++ Python/set_difference.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    n_students = int(input())
+    n_roll_nums = set(map(int, input().split()))
+    b_subs = int(input())
+    b_roll_nums = set(map(int, input().split()))
+
+    print(len(n_roll_nums.difference(b_roll_nums)))
